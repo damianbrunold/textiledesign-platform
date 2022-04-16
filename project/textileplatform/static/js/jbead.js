bf63217c1a08cb323f7c0695d4700b2ac55ecf0d
Damian Brunold <dab@dabsoft.ch>
2022-04-16 15:33:12 +0200

    Support get/save pattern for jbead type

--- project/textileplatform/static/js/jbead.js
+++ project/textileplatform/static/js/jbead.js
@@ -1,5 +1,8 @@
 "use strict";
 
 window.addEventListener("load", () => {
+    getPattern();
     document.getElementById("public").addEventListener("click", togglePublic);
+    document.getElementById("save").addEventListener("click", savePattern);
+    document.getElementById("close").addEventListener("click", function() { window.history.back() });
 });
