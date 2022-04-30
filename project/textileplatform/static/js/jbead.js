266aa22a825eba18fa6e7a261e71fd4bc7aa61a7
Damian Brunold <dab@dabsoft.ch>
2022-04-30 12:27:23 +0200

    Refactor jbead code to use model classes

--- project/textileplatform/static/js/jbead.js
+++ project/textileplatform/static/js/jbead.js
@@ -1,52 +1,206 @@
 "use strict";
 
-window.addEventListener("load", () => {
-    getPattern().then(init);
-    document.getElementById("public").addEventListener("click", togglePublic);
-    document.getElementById("save").addEventListener("click", savePattern);
-    document.getElementById("close").addEventListener("click", function() { window.history.back() });
-});
+// data is provided by common.js and contains the "raw" json data of the pattern.
+
+// The pattern is an instance of the Pattern class and encapsulates the 
+// data from the data object in order to manipulate and visualize it easier.
+let pattern = null;
 
 
+// The view is an instance of the View class and provides the visualization
+// of a pattern.
+let view = null;
+
+
+// The settings is an instance of the ViewSettings class and gathers drawing
+// related settings for all the views.
+let settings = null;
+
+
+// The colors variable represents the color palette
 let colors = [];
 
-let canvas = null;
-let ctx = null;
-let darcula = true;
 
-let gridw = 60;
-let gridh = 35;
+class Pattern {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.data = new Array(this.width * this.height);
+    }
 
-const dx = 12;
-const dy = 12;
+    idx(i, j) {
+        return i + j * this.width;
+    }
 
-const bx = dx * 0.15;
-const by = dy * 0.15;
+    get(i, j) {
+        return this.data[this.idx(i, j)];
+    }
 
-const offset_x = 0;
-const offset_y = 0;
-
-const pattern = new Array(gridw * gridh);
-
-const filled_style = darcula ? "#bbb" : "#000";
-
-for (let i = 0; i < pattern.length; i++) {
-    pattern[i] = -1;
+    set(i, j, value) {
+        this.data[this.idx(i, j)] = value;
+    }
 }
 
+
+class ViewDraft {
+    constructor(data, x, y, width, height) {
+        this.data = data;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.offset = 0;
+    }
+
+    draw(ctx, settings) {
+        this.drawGrid(ctx, settings);
+        this.drawData(ctx, settings);
+    }
+
+    drawGrid(ctx, settings) {
+        const dx = settings.dx;
+        const dy = settings.dy;
+
+        ctx.beginPath();
+        for (let i = 0; i <= this.width; i++) {
+            ctx.moveTo(0.5 + (this.x + i) * dx, 0.5);
+            ctx.lineTo(0.5 + (this.x + i) * dx, 0.5 + this.height * dy);
+        }
+        for (let j = 0; j <= this.height; j++) {
+            ctx.moveTo(0.5 + this.x * dx, 0.5 + j * dy);
+            ctx.lineTo(0.5 + (this.x + this.width) * dx, 0.5 + j * dy);
+        }
+        ctx.closePath();
+        ctx.strokeStyle = settings.darcula ? "#fff" : "#000";
+        ctx.lineWidth = 1.0;
+        ctx.stroke();
+    }
+
+    drawData(ctx, settings) {
+        const dx = settings.dx;
+        const dy = settings.dy;
+
+        for (let j = 0; j < this.height; j++) {
+            for (let i = 0; i < this.width; i++) {
+                const state = this.data.get(i, j);
+                if (state > 0) {
+                    ctx.fillStyle = colors[state];
+                    ctx.fillRect(
+                        0.5 + (this.x + i) * dx + 0.5,
+                        0.5 + (this.y + this.height - j - 1) * dy + 0.5,
+                        dx - 1,
+                        dy - 1);
+                }
+            }
+        }
+    }
+}
+
+
+class ViewCorrected {
+    constructor(data, x, y, width, height) {
+        this.data = data;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.offset = 0;
+        this.rotation = 0;
+    }
+
+    draw(ctx, settings) {
+        // TODO
+    }
+}
+
+
+class ViewSimulated {
+    constructor(data, x, y, width, height) {
+        this.data = data;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.offset = 0;
+    }
+
+    draw(ctx, settings) {
+        // TODO
+    }
+}
+
+
+class ViewSettings {
+    constructor(dx=12, dy=null) {
+        this.dx = dx;
+        this.dy = dy || this.dx;
+        this.darcula = true;
+    }
+}
+
+
+class PatternView {
+    constructor(pattern, settings, ctx) {
+        this.settings = settings;
+        this.pattern = pattern;
+        this.ctx = ctx;
+        this.layout();
+    }
+
+    layout() {
+        const dx = this.settings.dx;
+        const dy = this.settings.dy;
+
+        const availx = Math.trunc(this.ctx.canvas.width / dx);
+        const availy = Math.trunc(this.ctx.canvas.height / dy);
+
+        const width_ruler = 3;
+        const width_draft = this.pattern.width;
+        const width_corrected = this.pattern.width + 1;
+        const width_simulated = Math.trunc((this.pattern.width + 1) / 2);
+
+        const x1 = 0;
+        const x2 = width_ruler + 1;
+        const x3 = x2 + width_draft + 1;
+        const x4 = x3 + width_corrected + 1;
+        const x5 = x4 + width_simulated + 1;
+
+        this.draft = new ViewDraft(this.pattern, x2, 0, width_draft, availy);
+        this.corrected = new ViewCorrected(this.pattern, x3, 0, width_corrected, availy);
+        this.simulated = new ViewSimulated(this.pattern, x4, 0, width_simulated, availy);
+        // TODO bead-list
+    }
+
+    draw() {
+        this.clearCanvas();
+        this.draft.draw(this.ctx, this.settings);
+        this.corrected.draw(this.ctx, this.settings);
+        this.simulated.draw(this.ctx, this.settings);
+    }
+
+    clearCanvas() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+}
+
+
 function init() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    
-    canvas.style.backgroundColor = darcula ? "#333" : "#aaa";
+    pattern = new Pattern(data.model[0].length, data.model.length);
+    settings = new ViewSettings();
+    //settings.darcula = false;
 
     const container = document.getElementById("container");
+    const canvas = document.getElementById('canvas');
+    canvas.style.backgroundColor = settings.darcula ? "#000" : "#fff";
+    const ctx = canvas.getContext('2d');
+    ctx.canvas.width = container.clientWidth - 2;
+    ctx.canvas.height = container.clientHeight - 2;
 
-    gridw = Math.trunc((container.clientWidth - 1) / dx);
-    gridh = Math.trunc((container.clientHeight - 1) / dy);
+    view = new PatternView(pattern, settings, ctx);
 
-    ctx.canvas.width = gridw * dx + 1;
-    ctx.canvas.height = gridh * dy + 1;
+    initPattern(data, pattern);
+    
+    view.draw();
 
     canvas.addEventListener('click', function(event) {
         const x = event.offsetX;
@@ -56,24 +210,6 @@ function init() {
         toggle_pattern_at(pattern, i, j);
         repaint(ctx, pattern);
     });
-
-    init_example_pattern(pattern);
-    repaint(ctx, pattern);
-}
-
-function get_pattern_at(pattern, i, j) {
-    const idx = i + j * gridw;
-    return pattern[idx];
-}
-
-function set_pattern_at(pattern, i, j, value) {
-    const idx = i + j * gridw;
-    pattern[idx] = value;
-}
-
-function toggle_pattern_at(pattern, i, j) {
-    const idx = i + j * gridw;
-    pattern[idx] = - pattern[idx];
 }
 
 function draw_grid(ctx) {
@@ -87,51 +223,30 @@ function draw_grid(ctx) {
         ctx.lineTo(offset_x + 0.5 + gridw * dx, offset_y + 0.5 + j * dy);
     }
     ctx.closePath();
-    ctx.strokeStyle = darcula ? "#aaa" : "#000";
+    ctx.strokeStyle = darcula ? "#fff" : "#000";
     ctx.lineWidth = 1.0;
     ctx.stroke();
 }
 
-function draw_cell(ctx, i, j, state) {
-    if (state > 0) {
-        ctx.fillStyle = colors[state];
-        ctx.fillRect(
-            offset_x + 0.5 + i * dx + 1, 
-            offset_y + 0.5 + j * dy + 1, 
-            dx - 1, 
-            dy - 1);
-    }
-}
-
-function draw_pattern(ctx, pattern) {
-    for (let i = 0; i < gridw; i++) {
-        for (let j = 0; j < gridh; j++) {
-            draw_cell(ctx, i, gridh - j - 1, get_pattern_at(pattern, i, j));
-        }
-    }
-}
-
-function clear_canvas(ctx) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
-
-function repaint(ctx, pattern) {
-    clear_canvas(ctx);
-    draw_grid(ctx);
-    draw_pattern(ctx, pattern);
-}
-
-function init_example_pattern(pattern) {
+function initPattern(data, pattern) {
     console.log(data);
     for (let i = 0; i < data.colors.length; i++) {
         const spec = data.colors[i];
         colors.push(`rgb(${spec[0]}, ${spec[1]}, ${spec[2]})`);
     }
-    for (let j = 0; j < gridh; j++) {
+    for (let j = 0; j < pattern.height; j++) {
         const row = data.model[j];
         for (let i = 0; i < row.length; i++) {
-            set_pattern_at(pattern, i, j, row[i]);
+            pattern.set(i, j, row[i]);
         }
     }
 }
 
+window.addEventListener("load", () => {
+    getPattern().then(init);
+    document.getElementById("public").addEventListener("click", togglePublic);
+    document.getElementById("save").addEventListener("click", savePattern);
+    document.getElementById("close").addEventListener("click", closePattern);
+});
+
+window.addEventListener("resize", resizeWindow);
