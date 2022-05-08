7a2e5b41011b71b74c73c29f334c9987a7e3f592
Damian Brunold <dab@dabsoft.ch>
2022-05-08 20:00:03 +0200

    Bump version

--- project/setup.py
+++ project/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='textileplatform',
-    version='0.0.5',
+    version='0.0.7',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
