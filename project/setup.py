76728001f0d2942e7bea178a8d67837b99ff53c8
Damian Brunold <dab@dabsoft.ch>
2022-07-17 14:25:16 +0200

    Switch to declarative setup format

--- project/setup.py
+++ project/setup.py
@@ -1,17 +1,2 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='textileplatform',
-    version='0.0.28',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'flask',
-        'flask-babel',
-        'pg8000',
-        'SQLAlchemy',
-        'reportlab',
-        'Pillow'
-    ],
-)
+from setuptools import setup
+setup()
