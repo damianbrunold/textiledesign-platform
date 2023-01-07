7ce340619e609d8f0a03adf9aea06e98350ce322
Damian Brunold <dab@dabsoft.ch>
2023-01-07 19:57:10 +0100

    Add automatic renaming when adding patterns with duplicate names

--- project/textileplatform/persistence.py
+++ project/textileplatform/persistence.py
@@ -13,35 +13,71 @@ def clean_name(name):
 
 
 def add_weave_pattern(pattern, user_name):
+    suffix = None
     now = datetime.datetime.utcnow()
-    db.session.add(Pattern(
-        name=clean_name(pattern['name']),
-        label=pattern['name'],
-        owner=user_name,
-        pattern_type="DB-WEAVE Pattern",
-        description=pattern['notes'],
-        contents=json.dumps(pattern),
-        created=now,
-        modified=now,
-        public=False
-    ))
-    db.session.commit()
+    while True:
+        if suffix and suffix > 10:
+            break
+        if suffix:
+            name = clean_name(pattern["name"] + " - " + str(suffix))
+            label = pattern["name"] + " - " + str(suffix)
+        else:
+            name = clean_name(pattern["name"])
+            label = pattern["name"]
+        try:
+            db.session.add(Pattern(
+                name=name,
+                label=label,
+                owner=user_name,
+                pattern_type="DB-WEAVE Pattern",
+                description=pattern["notes"],
+                contents=json.dumps(pattern),
+                created=now,
+                modified=now,
+                public=False
+            ))
+            db.session.commit()
+            break
+        except IntegrityError:
+            db.session.rollback()
+            if suffix:
+                suffix += 1
+            else:
+                suffix = 1
 
 
 def add_bead_pattern(pattern, user_name):
+    suffix = None
     now = datetime.datetime.utcnow()
-    db.session.add(Pattern(
-        name=clean_name(pattern['name']),
-        label=pattern['name'],
-        owner=user_name,
-        pattern_type="JBead Pattern",
-        description=pattern['notes'],
-        contents=json.dumps(pattern),
-        created=now,
-        modified=now,
-        public=False,
-    ))
-    db.session.commit()
+    while True:
+        if suffix and suffix > 10:
+            break
+        if suffix:
+            name = clean_name(pattern["name"] + " - " + str(suffix))
+            label = pattern["name"] + " - " + str(suffix)
+        else:
+            name = clean_name(pattern["name"])
+            label = pattern["name"]
+        try:
+            db.session.add(Pattern(
+                name=name,
+                label=label,
+                owner=user_name,
+                pattern_type="JBead Pattern",
+                description=pattern["notes"],
+                contents=json.dumps(pattern),
+                created=now,
+                modified=now,
+                public=False,
+            ))
+            db.session.commit()
+            break
+        except IntegrityError:
+            db.session.rollback()
+            if suffix:
+                suffix += 1
+            else:
+                suffix = 1
 
 
 def get_patterns_for_user_name(user_name, only_public=False):
@@ -66,10 +102,10 @@ def clone_pattern(user_name, pattern, contents):
         try:
             now = datetime.datetime.utcnow()
             if suffix:
-                name = pattern.name + " - " + str(suffix)
+                name = clean_name(pattern.name + " - " + str(suffix))
                 label = pattern.label + " - " + str(suffix)
             else:
-                name = pattern.name
+                name = clean_name(pattern.name)
                 label = pattern.label
             db.session.add(Pattern(
                 name=name,
